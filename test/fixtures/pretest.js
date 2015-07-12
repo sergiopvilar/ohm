4607d3cfa81fce0105fb6ccd4f20c110d42f9721
Sérgio Vilar <sergiovilar.r@gmail.com>
2015-07-12 00:37:49 -0300

    Fixed pretest script

--- test/fixtures/pretest.js
+++ test/fixtures/pretest.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 
-fs.unlinkSync(__dirname + '/Ohm.db');
+if(fs.existsSync(__dirname + '/Ohm.db'))
+  fs.unlinkSync(__dirname + '/Ohm.db');
 fs.createReadStream(__dirname + '/Test.db')
   .pipe(fs.createWriteStream(__dirname + '/Ohm.db'));
